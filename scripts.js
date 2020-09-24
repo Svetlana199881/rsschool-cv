06a6041f624d82c49696f140135ba60f61f90c29
Svetlana199881 <sandreeevna@gmail.com>
2020-09-24 21:00:31 +0300

    feat: function for btn

--- scripts.js
+++ scripts.js
@@ -1,31 +1,44 @@
 let numbers = document.querySelectorAll('.number');
 let operations = document.querySelectorAll('.operator');
+let clearBtns = document.querySelectorAll('.clear-btn');
 let decimalB = document.getElementById('decimal');
-let ce = document.getElementById('ce');
-let c = document.getElementById('c');
 let result = document.getElementById('result');
 
-for (var i = 0; i < numbers.length; i++) {
-    var number = number [i];
-    number.addEventListener('click', function (e) {
-        console.log('Клик по кнопке с номером!');
-    });
+for (let i = 0; i < numbers.length; i++) {
+    let number = numbers [i];
+    number.addEventListener('click',  numberPress); 
 };
 
-for (var i = 0; i < operations.length; i++) {
-    var operation = operations [i];
-    operation.addEventListener('click', function (e) {
-        console.log('Клик по кнопке с операцией!');
-    });
+for (let i = 0; i < operations.length; i++) {
+    let operationBtn = operations [i];
+    operationBtn.addEventListener('click', operation);
 };
 
+for (let i = 0; i < clearBtns.length; i++) {
+	let clearBtn = clearBtns [i];
+	clearBtn.addEventListener('click', function (e) {
+		console.log(e.srcElement.id);
+		console.log('Клик по кнопке с CE или C!');
+	});
+};
 
+result.addEventListener('click', function (e) {
+    console.log('Клик по кнопке с C!');
+});
+
+decimalB.addEventListener('click', function (e) {
+    console.log('Клик по кнопке с десятичной дробью!');
+});
 
 function numberPress(){
-
+    console.log('Клик по кнопке с номером!');
 };
 
 function operation(argument) {
+	console.log('Клик по кнопке с операцией!');
+};
+
+function clear(argument) {
 
 };
 
@@ -33,8 +46,5 @@ function decimal(argument) {
 
 };
 
-function clear(argument) {
-
-};
 
 
