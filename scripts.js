285b0358079a45d2cc4f140b6935d057dcbd1cf1
Svetlana199881 <sandreeevna@gmail.com>
2020-09-24 21:52:14 +0300

    feat: event click

--- scripts.js
+++ scripts.js
@@ -2,49 +2,51 @@ let numbers = document.querySelectorAll('.number');
 let operations = document.querySelectorAll('.operator');
 let clearBtns = document.querySelectorAll('.clear-btn');
 let decimalB = document.getElementById('decimal');
-let result = document.getElementById('result');
+let resultBtn = document.getElementById('result');
 
 for (let i = 0; i < numbers.length; i++) {
     let number = numbers [i];
-    number.addEventListener('click',  numberPress); 
+		number.addEventListener('click',  function (e) {
+			numberPress(e.target.textContent);
+		}); 
 };
 
 for (let i = 0; i < operations.length; i++) {
     let operationBtn = operations [i];
-    operationBtn.addEventListener('click', operation);
+    operationBtn.addEventListener('click', function (e) {
+			console.log(e.target.textContent);
+			operation();
+		});
 };
 
 for (let i = 0; i < clearBtns.length; i++) {
 	let clearBtn = clearBtns [i];
 	clearBtn.addEventListener('click', function (e) {
-		console.log(e.srcElement.id);
-		console.log('Клик по кнопке с CE или C!');
+		clear(e.srcElement.id)
 	});
 };
 
-result.addEventListener('click', function (e) {
-    console.log('Клик по кнопке с C!');
-});
+resultBtn.addEventListener('click', result);
 
-decimalB.addEventListener('click', function (e) {
-    console.log('Клик по кнопке с десятичной дробью!');
-});
+decimalB.addEventListener('click', decimal);
 
-function numberPress(){
-    console.log('Клик по кнопке с номером!');
+function numberPress(number){
+    console.log('Клик по кнопке с номером ' + number + '!');
 };
 
 function operation(argument) {
 	console.log('Клик по кнопке с операцией!');
 };
 
-function clear(argument) {
-
+function clear(id) {
+	console.log('Клик по кнопке ' + id + '!');
 };
 
 function decimal(argument) {
-
+	console.log('Клик по кнопке с десятичной дробью!');
 };
 
-
+function result(argument) {
+	console.log('Клик по кнопке с результатом!');
+}
 
