317508f1de71b0097370e6f2ae99e03d3d627a27
Svetlana199881 <sandreeevna@gmail.com>
2020-09-24 20:24:42 +0300

    feat: add  let

---
+++ scripts.js
@@ -0,0 +1,40 @@
+let numbers = document.querySelectorAll('.number');
+let operations = document.querySelectorAll('.operator');
+let decimalB = document.getElementById('decimal');
+let ce = document.getElementById('ce');
+let c = document.getElementById('c');
+let result = document.getElementById('result');
+
+for (var i = 0; i < numbers.length; i++) {
+    var number = number [i];
+    number.addEventListener('click', function (e) {
+        console.log('Клик по кнопке с номером!');
+    });
+};
+
+for (var i = 0; i < operations.length; i++) {
+    var operation = operations [i];
+    operation.addEventListener('click', function (e) {
+        console.log('Клик по кнопке с операцией!');
+    });
+};
+
+
+
+function numberPress(){
+
+};
+
+function operation(argument) {
+
+};
+
+function decimal(argument) {
+
+};
+
+function clear(argument) {
+
+};
+
+
